4114220e7229399a99e66dcd836fdf5baeeddf39
pjantalo <68040337@kmitl.ac.th>
2025-06-17 22:56:26 +0700

    Create whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+firstname = input("What's your first name?: ")
+lastname = input("And your last name?: ")
+print("Well, pleased to meet you,", firstname + " " +lastname)
+# Output: 
+# What's your first name?: Panadda
+# And your last name?:Jantalok
+# Well, pleased to meet you, Panadda Jantalok
