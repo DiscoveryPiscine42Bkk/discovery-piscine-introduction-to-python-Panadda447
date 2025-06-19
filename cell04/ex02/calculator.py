5b26a39bc542784356570995727d9bca3f328679
pjantalo <68040337@kmitl.ac.th>
2025-06-19 11:32:37 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#calculator.py
+num1 = int(input("Give the first number: "))
+num2 = int(input("Give the second number: "))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print(f"{num1} + {num2} = {add}")
+print(f"{num1} - {num2} = {sub}")
+print(f"{num1} * {num2} = {mul}")
+print(f"{num1} / {num2} = {div}")
